da4d1df60610ae5bfe041e87f6e3b98787f9e8d6
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2019-03-14 13:51:54 +0000

    Multimoneda

--- models/account.py
+++ models/account.py
@@ -6,7 +6,6 @@ from openerp.exceptions import UserError, ValidationError
 from datetime import datetime
 import base64
 import zeep
-
 import logging
 
 class AccountInvoice(models.Model):
@@ -24,22 +23,23 @@ class AccountInvoice(models.Model):
                 for linea in factura.invoice_line_ids:
                     det = {}
 
-                    det["detalleImpuestosIva"] = 0
-                    r = linea.invoice_line_tax_ids.compute_all(linea.price_unit, currency=factura.currency_id, quantity=linea.quantity, product=linea.product_id, partner=factura.partner_id)
-                    for impuestos in r['taxes']:
-                        det["detalleImpuestosIva"] = impuestos['amount']
-
                     det["unidadMedida"] = "UND"
                     det["cantidad"] = linea.quantity
                     det["codigoProducto"] = linea.product_id.default_code
                     det["descripcionProducto"] = linea.name
-                    det["precioUnitario"] = linea.price_unit
-                    det["montoBruto"] = linea.price_subtotal
-                    det["montoDescuento"] = (linea.price_unit * linea.quantity) * (linea.discount / 100)
+                    det["precioUnitario"] = linea.price_unit * ( 1 - linea.discount / 100 )
+                    det["montoBruto"] = linea.price_subtotal * det["cantidad"]
+                    # det["montoDescuento"] = (linea.price_unit * linea.quantity) * (linea.discount / 100)
+                    det["montoDescuento"] = 0
                     det["importeNetoGravado"] = det["cantidad"] * det["precioUnitario"]
                     det["importeExento"] = 0
                     det["importeOtrosImpuestos"] = 0
                     det["importeTotalOperacion"] = det["cantidad"] * det["precioUnitario"]
+                    det["detalleImpuestosIva"] = 0
+                    r = linea.invoice_line_tax_ids.compute_all(det["precioUnitario"], currency=factura.currency_id, quantity=det["cantidad"], product=linea.product_id, partner=factura.partner_id)
+                    for impuestos in r['taxes']:
+                        det["detalleImpuestosIva"] = impuestos['amount']
+
                     if linea.product_id.type == "service":
                         det["tipoProducto"] = "S"
                     else:
@@ -59,7 +59,16 @@ class AccountInvoice(models.Model):
                 dte["dte"]["numeroDocumento"] = "odoo_aquih_"+str(factura.id)
                 dte["dte"]["serieAutorizada"] = factura.journal_id.serie_gface
                 dte["dte"]["codigoMoneda"] = "GTQ"
+                if factura.currency_id.id != factura.company_id.currency_id.id:
+                    dte["dte"]["codigoMoneda"] = "USD"
                 dte["dte"]["tipoCambio"] = 1
+                if factura.currency_id.id != factura.company_id.currency_id.id:
+                    total = 0
+                    for l in factura.move_id.line_ids:
+                        if l.account_id.id == factura.account_id.id:
+                            total += l.debit - l.credit
+                    tipo_cambio = abs(total / factura.amount_total)
+                    dte["dte"]["tipoCambio"] = tipo_cambio
                 dte["dte"]["regimen2989"] = False
                 dte["dte"]["regimenISR"] = "RET_DEFINITIVA"
                 dte["dte"]["correoComprador"] = "N/A"
@@ -114,7 +123,7 @@ class AccountJournal(models.Model):
     clave_gface = fields.Char('Clave GFACE', copy=False)
     nombre_establecimiento_gface = fields.Char('Nombre Establecimiento GFACE', copy=False)
     tipo_documento_gface = fields.Selection([('FACE', 'FACE')], 'Tipo de Documento GFACE', copy=False)
-    serie_documento_gface = fields.Selection([('63', '63')], 'Serie de Documento GFACE', copy=False)
+    serie_documento_gface = fields.Selection([('63', '63'),('66', '66')], 'Serie de Documento GFACE', copy=False)
     serie_gface = fields.Char('Serie GFACE', copy=False)
     numero_resolucion_gface = fields.Char('Numero Resolucion GFACE', copy=False)
     fecha_resolucion_gface = fields.Date('Fecha Resolución GFACE', copy=False)
